82ced3a13f4379bf2b528c867478f281375bf2cb
AnaMaria <aungureanu614@gmail.com>
2016-09-18 01:43:05 +0000

    all

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://test:test123@ds033116.mlab.com:33116/mongo-shopping-list-aungureanu');
+                            'mongodb://test:test123@ds033116.mlab.com:33116/mongo-shopping-list-aungureanu':
+                            'mongodb://localhost/shopping-list' );
 exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
 var mongoose = require('mongoose');
 
 var config = require('./config');
