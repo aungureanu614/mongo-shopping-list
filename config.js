bfe40f76eeabb341c4bf8622d4a868b207d3321e
AnaMaria <aungureanu614@gmail.com>
2016-09-18 00:47:22 +0000

    travis-config

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
                        (process.env.NODE_ENV === 'production' ?
                             'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            'mongodb://test:test123@ds033116.mlab.com:33116/mongo-shopping-list-aungureanu');
 exports.PORT = process.env.PORT || 8080;
